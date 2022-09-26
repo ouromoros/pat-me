dfc8f941cb2addfbc957633fa87d1125e78197ea
ouromoros <ouromoros@gmail.com>
2022-09-26 15:58:36 +0800

    Add env unittest

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,6 +1,7 @@
 use assert_cmd::prelude::*; // Add methods on commands
 use predicates::prelude::*; // Used for writing assertions
 use std::process::Command; // Run programs
+use std::env;
 
 #[test]
 fn help() -> Result<(), Box<dyn std::error::Error>> {
@@ -8,6 +9,24 @@ fn help() -> Result<(), Box<dyn std::error::Error>> {
 
     cmd.arg("-h");
     cmd.assert().success();
+
+    Ok(())
+}
+
+#[test]
+#[cfg(target_os = "windows")]
+fn inherit_env() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
+
+    let var_name = "variableA";
+    let var_value = "Hellow";
+    env::set_var(var_name, var_value);
+
+    cmd.arg("-c")
+       .arg("echo $env:variableA")
+       .arg("echo")
+       .assert()
+       .stdout(predicate::str::contains(var_value));
 
     Ok(())
 }
